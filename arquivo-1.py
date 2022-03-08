25d49e2c90be36cf785767e7567e6a50e93e8ea0
Leandro Antonio <eng.leandro.a@gmail.com>
2022-03-08 10:26:48 -0300

    criado arquivo celsius para fahrenheit e renomeado arquivo_1 com underscore recomendado pelo pep8

--- arquivo-1.py
+++
@@ -1,6 +0,0 @@
-print('Olá Mundo')
-a = 1
-b = 2
-c = a + b
-print(c)
-# teste de funcionamento do e configuração do ambiente IDE com o github
---
+++ celsius_para_fahrenheit.py
@@ -0,0 +1,7 @@
+"""Recebe um valor em Celsius e devolve em Fahrenheit"""
+
+C = int(input('Digite um valor em Celcius:'))
+print(C)
+F = C*(9/5)
+print(F)
+print(f'O valor {C} graus Celsius é {F} graus Fahrenheit.')
